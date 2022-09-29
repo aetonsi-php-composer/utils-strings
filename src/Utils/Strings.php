d73326c0eaa85743ff64647b56babcf4edcf7e80
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-09-29 12:45:36 +0200

    add function htmlEntitiesFull

--- src/Utils/Strings.php
+++ src/Utils/Strings.php
@@ -28,4 +28,43 @@ class Strings
         }
         return \implode('', $pieces);
     }
+
+
+    /**
+     * Equivalent of htmlentities, that also converts to html numeric entities to non-ascii characters that have no named entity. It can effectively be used for any unicode text.
+     *
+     * Adapted from: https://stackoverflow.com/a/3005240
+     *
+     * @param string $str input string
+     * @param bool $forceNumericEntities if true, ALL the characters are converted to html numeric entities
+     * @return string
+     */
+    public static function htmlEntitiesFull($str, $forceNumericEntities = false)
+    {
+        $str = \mb_convert_encoding($str, 'UTF-32', 'UTF-8');
+        $codepoints = \unpack('N*', $str);
+        $result = \array_map(function ($codepoint) use ($forceNumericEntities) {
+            if ($forceNumericEntities) {
+                // convert ALL characters to html numeric entities
+                $result =  "&#$codepoint;";
+            } else {
+                $character = \mb_convert_encoding("&#$codepoint;", 'UTF-8', 'HTML-ENTITIES');
+                $characterHtmlNamedEntity = \htmlentities($character, \ENT_QUOTES);
+                if ($characterHtmlNamedEntity !== $character) {
+                    // html named entity escape exists! using it
+                    $result = $characterHtmlNamedEntity;
+                } elseif (\mb_check_encoding($character, 'ASCII')) { // OR $codepoint < 128
+                    // is ascii? just use the character
+                    $result = $character;
+                } else {
+                    // utf8 entity, not ascii, without html named escape sequence; use the html numeric entity
+                    $result = "&#$codepoint;";
+                }
+            }
+
+            return $result;
+        }, $codepoints);
+
+        return \implode('', $result);
+    }
 }
